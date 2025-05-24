7f459998e3b26acbb6f4696ca6a0edf87550db18
brgsfrmhll <br1n0@live.com>
2025-05-24 07:23:07 -0300

    Update indicadores_scpc.py

--- indicadores_scpc.py
+++ indicadores_scpc.py
@@ -1478,11 +1478,11 @@ def edit_indicator(SETORES, TIPOS_GRAFICOS, INDICATORS_FILE, INDICATOR_LOG_FILE,
             st.session_state.current_var_descriptions = {}
             st.session_state.current_variable_values = {} # Limpa também os valores de variáveis
 
-            time.sleep(2) 
-            scroll_to_top() 
+            
             st.rerun()
 
-
+    time.sleep(20) 
+    scroll_to_top() 
     st.markdown('</div>', unsafe_allow_html=True)
      
 def display_result_with_delete(result, selected_indicator, RESULTS_FILE, USER_LOG_FILE):
@@ -2052,7 +2052,7 @@ def fill_indicator(SETORES, INDICATORS_FILE, RESULTS_FILE, TEMA_PADRAO, USER_LOG
             else:
                 st.info("Nenhum registro de preenchimento encontrado para este indicador.")
 
-        time.sleep(2) 
+        time.sleep(20) 
         scroll_to_top() 
     st.markdown('</div>', unsafe_allow_html=True)
     
